a843ee0f3b0717ad61cfe64211daaa9e33b36ce4
tampieri_samuele <tampieri.samuele@iisgalvanimi.edu.it>
2026-02-26 17:29:05 +0000

    creazione componenti

---
+++ RecuperoVerifica/src/app/fourth/fourth.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth',
+  imports: [],
+  templateUrl: './fourth.html',
+  styleUrl: './fourth.css',
+})
+export class Fourth {}
---
+++ RecuperoVerifica/src/app/third/third.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third',
+  imports: [],
+  templateUrl: './third.html',
+  styleUrl: './third.css',
+})
+export class Third {}
